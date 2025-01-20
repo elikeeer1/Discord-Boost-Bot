9c2d7de7ab79bfc57690cda74d0d5165790cd3ac
elikeeer1 <ikersinho2@gmail.com>
2025-01-20 20:03:35 +0100

    Update boost.py

--- boost.py
+++ boost.py
@@ -29,7 +29,7 @@ clear()
 async def join_server(token, inv):
   headers = {"Authorization": token, "accept": "*/*", "accept-language": "en-US", "connection": "keep-alive", "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US', "DNT": "1", "origin": "https://discord.com", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "referer": "https://discord.com/channels/@me", "TE": "Trailers", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36", "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"}
   async with aiohttp.ClientSession(headers=headers) as serverjoinersession:
-    async with serverjoinersession.post(f"https://discord.com/api/v9/invites/{inv}") as response:
+    async with serverjoinersession.post(f"https://discord.com/api/v9/invites/{inv}") as response:https://discord.gg/e5HgWyWF
       if response.status in (204, 200, 201):
         print(f"[-] Blust#9380 | Successfully Joined Server")
       else:
@@ -55,7 +55,7 @@ async def boost_server(guildid, token):
 banner = f"""{Fore.RED}[-]{Fore.RESET} Created By Blust#9380\n\n{Fore.BLUE}[1]{Fore.RESET} Server Joiner\n\n{Fore.BLUE}[2]{Fore.RESET} Boost Server\n"""
 
 
-async def start_join(inv):
+async def start_join(inv):https://discord.gg/e5HgWyWF
   async with tasksio.TaskPool(10_000) as pool:
     for token in open('tokens.txt', 'r').readlines():
       tk = token.strip()
